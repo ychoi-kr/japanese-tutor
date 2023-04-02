9c21f3a9bdb853da429ad9187a4e550d37205745
Yong Choi <ychoi_kr@naver.com>
2023-04-02 23:45:21 +0900

    feat: add translate()

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import util
+from util import *
+import datetime
+import re
 
 import speech_recognition as sr
 import whisper
@@ -13,9 +15,10 @@ import playsound
 class ChatApp:
     def __init__(self):
         openai.api_key = os.getenv("OPENAI_API_KEY")
-        self.format = "今朝、新宿駅で友達に会いました。彼女は外国人で、日本語が上手です。 (오늘 아침, 신주쿠 역에서 친구를 만났습니다. 그녀는 외국인이며 일본어가 능숙합니다.)"
+
+        instruction = f"당신은 지금부터 일본어 선생님입니다. 저와 가벼운 대화를 하면서 자연스럽게 일본어를 익히도록 도와주세요. 지금은 {formatted_time()}입니다. 먼저 인사를 건네고, 일상적인 질문으로 대화를 이끌어 주세요."
         self.messages = [
-            {"role": "system", "content": "당신은 지금부터 일본어 선생님입니다. 저와 가벼운 대화를 하면서 자연스럽게 일본어를 익히도록 도와주세요. 다음 형식으로 말씀해 주세요.\n\n---\n\n" + self.format + "\n\n---\n\n먼저 인사를 건네고, 일상적인 질문으로 대화를 이끌어 주세요."},
+            {"role": "system", "content": self.translate(instruction, 'JA')},
         ]
 
 
@@ -30,49 +33,30 @@ class ChatApp:
         if len(self.messages) > memory:
             self.messages = [self.messages[0]] + self.messages[1 - memory : -1]
         self.messages.append({"role": "assistant", "content": response["choices"][0]["message"].content})
-        return self.extract_contents(response["choices"][0]["message"]["content"])
+        return response["choices"][0]["message"]["content"]
 
 
-    def extract_contents(self, text):
-        lines = text.strip().split('\n')
-        contents = {}
-        current_key = "UNK"
-    
-        for line in lines:
-            if '(' in line:
-                contents["JA"], contents["KO"] = util.split_text_by_brackets(line)
-            elif util.contains_japanese(line):
-                contents["JA"] = line
-            elif util.contains_korean(line):
-                contents["KO"] = line
-            else:
-                contents["UNK"] = line
-    
-        return contents
+    def translate(self, text, lang):
+        prompt = f'Translate into {lang}:\n\n{text}'
+
+        response = openai.Completion.create(
+          model="text-davinci-003",
+          prompt=prompt,
+          temperature=0.3,
+          max_tokens=1000,
+          top_p=1.0,
+          frequency_penalty=0.0,
+          presence_penalty=0.0
+        )
+        return response['choices'][0]['text'].strip()
 
 
-    def speak(self, answer):
-        def play(text, lang):
-            tts = gTTS(text, lang=lang)
-            filename = "speaking.mp3"
-            tts.save(filename)
-            playsound.playsound(filename)
-            os.remove(filename)
-        
-        if 'JA' in answer and 'KO' in answer:
-            print(f"先生: {answer['JA']} (선생님: {answer['KO']})")
-            play(answer['JA'], 'ja')
-        elif 'JA' in answer:
-            print(f"先生: {answer['JA']}")
-            play(answer['JA'], 'ja')
-        elif 'KO' in answer:
-            print('선생님: ' + answer['KO'])
-        elif 'UNK' in answer:
-            print('선생님: ' + answer['UNK'])
-        else:
-            pass
-
-        print('')
+    def speak(self, text):
+        tts = gTTS(text, lang='ja')
+        filename = "speaking.mp3"
+        tts.save(filename)
+        playsound.playsound(filename)
+        os.remove(filename)
     
     
     def listen(self):
@@ -88,37 +72,37 @@ class ChatApp:
 
             audio = r.listen(source)
             audio_data = torch.from_numpy(np.frombuffer(audio.get_raw_data(), np.int16).flatten().astype(np.float32) / 32768.0)
-            result = audio_model.transcribe(audio_data, fp16=False)
+            text = audio_model.transcribe(audio_data, fp16=False)["text"]
     
             print(' ' * (len(prmpt) + 3), end='\r')
-            return result["text"].ljust(len(prmpt))
+            return text.ljust(len(prmpt))
 
 
 def main():
 
     app = ChatApp()
     tutors_word = app.chat('')
+    print('先生: ' + tutors_word)
     app.speak(tutors_word)
+    print('선생님: ' + app.translate(tutors_word, 'KO') + '\n')
 
     while True:
         my_word = app.listen().lstrip()
+        
         print('나: ' + my_word + '\n')
 
         graceful_exit = 0
-        if my_word.strip() == 'さようなら':
+        if re.search('さようなら|おやすみなさい', my_word):
             graceful_exit = 2
 
         tutors_word = app.chat(my_word)
+        print('先生: ' + tutors_word)
         app.speak(tutors_word)
+        print('선생님: ' + app.translate(tutors_word, 'KO') + '\n')
 
-        s = ''.join([value for value in tutors_word.values()])
         if graceful_exit:
             graceful_exit -= 1
-            if (
-               'さようなら' in s or
-               'またいつかお話しましょう' in s or
-               'また今度お話しましょうね' in s
-            ):
+            if re.search('さようなら|またいつかお話しましょう|また今度お話しましょうね|おやすみなさい', tutors_word):
                 print('프로그램을 종료합니다.')
                 break
 
